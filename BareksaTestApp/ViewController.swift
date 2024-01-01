8b3b6f4667ed0e174408dd08dc508a408b17cfd1
jerrypm <jerrypurnamamaulid@gmail.com>
2024-01-01 13:24:07 +0700

    Initial Commit

---
+++ BareksaTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BareksaTestApp
+//
+//  Created by Jeri Purnama Maulid on 01/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
